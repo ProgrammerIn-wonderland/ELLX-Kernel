2e8c2d99262435510fb9b1f48f4637a4b9149099
ProgrammerIn-wonderland <3838shah@gmail.com>
2026-03-22 20:24:36 -0400

    Add in Romulus ath12k enumeration fix

--- drivers/net/wireless/ath/ath12k/core.c
+++ drivers/net/wireless/ath/ath12k/core.c
@@ -76,6 +76,8 @@ static int ath12k_core_rfkill_config(struct ath12k_base *ab)
 
 	if (!(ab->target_caps.sys_cap_info & WMI_SYS_CAP_INFO_RFKILL))
 		return 0;
+        
+	return 0; // Microsoft Surface Laptop 7 Enumeration hack
 
 	if (ath12k_acpi_get_disable_rfkill(ab))
 		return 0;
